5c58559dede8a4884ac1a4acf79ac043a0560a59
alihkdeveloper <alihkdeveloper@gmail.com>
2023-06-03 17:01:07 +0400

    first push

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<HelloWorld />
+
+	<h3 id="location"></h3>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+	name: "App",
+	data() {
+		return {
+			longitude: null,
+			latitude: null,
+		};
+	},
+	components: {
+		HelloWorld,
+	},
+	created() {
+		const success = (position) => {
+			const getLatitude = position.coords.latitude;
+			const getLongitude = position.coords.longitude;
+			document.getElementById(
+				"location"
+			).innerText = `Latitude: ${getLatitude} - Longitude: ${getLongitude}`;
+			// Do something with the position
+		};
+
+		const error = (err) => {
+			console.log(err);
+		};
+
+		// This will open permission popup
+		navigator.geolocation.getCurrentPosition(success, error);
+	},
+};
+</script>
